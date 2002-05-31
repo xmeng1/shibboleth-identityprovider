02981b3973a12f3f2433052ea5ce538ce2aab5cc
wassa <wassa@ab3bd59b-922f-494d-bb5f-6f0a3c29deca>
2002-05-31 00:25:43 +0000

    Housekeeping on WAYF javadoc.
    git-svn-id: https://subversion.switch.ch/svn/shibboleth/java-idp/trunk@14 ab3bd59b-922f-494d-bb5f-6f0a3c29deca

--- src/edu/internet2/middleware/shibboleth/wayf/WayfCacheBase.java
+++ src/edu/internet2/middleware/shibboleth/wayf/WayfCacheBase.java
@@ -3,12 +3,9 @@ package edu.internet2.middleware.shibboleth.wayf;
 import javax.servlet.http.HttpServletRequest;
 
 /**
- * @author Administrator
- *
- * To change this generated comment edit the template variable "typecomment":
- * Window>Preferences>Java>Templates.
- * To enable and disable the creation of type comments go to
- * Window>Preferences>Java>Code Generation.
+ * Shared implementation code for <code>WayfCache</code>.
+ * 
+ * @author Walter Hoehn wassa&#064;columbia.edu
  */
 public abstract class WayfCacheBase implements WayfCache {
 
