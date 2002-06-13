9ab4c140b9ded234e4808fbe958ef35331f1c98f
wassa <wassa@ab3bd59b-922f-494d-bb5f-6f0a3c29deca>
2002-06-13 18:32:00 +0000

    Added common log4j initialization.
    git-svn-id: https://subversion.switch.ch/svn/shibboleth/java-idp/trunk@104 ab3bd59b-922f-494d-bb5f-6f0a3c29deca

---
+++ src/edu/internet2/middleware/shibboleth/log/LoggerInit.java
@@ -0,0 +1,38 @@
+package edu.internet2.middleware.shibboleth.log;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.apache.xml.security.Init;
+
+/**
+ * 
+ * Servlet used to configure logging for other components.
+ * 
+ * @author Walter Hoehn
+ *
+ */
+public class LoggerInit extends HttpServlet {
+
+	private static Logger log = Logger.getLogger(LoggerInit.class.getName());
+
+	/**
+	 * @see javax.servlet.GenericServlet#init()
+	 */
+	public void init() throws ServletException {
+
+		super.init();
+		//Silliness to get around xmlsec doing its own configuration, ie: we might need to override it
+		Init.init();
+
+		String log4jConfigFileLocation = getServletConfig().getInitParameter("log4jConfigFileLocation");
+		if (log4jConfigFileLocation == null) {
+			log4jConfigFileLocation = "/WEB-INF/conf/log4j.properties";
+		}
+		PropertyConfigurator.configure(getServletContext().getRealPath("/") + log4jConfigFileLocation);
+	}
+
+}
+
