d043b8b480d09949693949886e12072b093b210c
wassa <wassa@ab3bd59b-922f-494d-bb5f-6f0a3c29deca>
2002-06-25 04:46:11 +0000

    Added Shibboleth license to java source files.
    git-svn-id: https://subversion.switch.ch/svn/shibboleth/java-idp/trunk@188 ab3bd59b-922f-494d-bb5f-6f0a3c29deca

--- src/edu/internet2/middleware/shibboleth/wayf/CookieWayfCache.java
+++ src/edu/internet2/middleware/shibboleth/wayf/CookieWayfCache.java
@@ -1,3 +1,52 @@
+/* 
+ * The Shibboleth License, Version 1. 
+ * Copyright (c) 2002 
+ * University Corporation for Advanced Internet Development, Inc. 
+ * All rights reserved
+ * 
+ * 
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this 
+ * list of conditions and the following disclaimer.
+ * 
+ * Redistributions in binary form must reproduce the above copyright notice, 
+ * this list of conditions and the following disclaimer in the documentation 
+ * and/or other materials provided with the distribution, if any, must include 
+ * the following acknowledgment: "This product includes software developed by 
+ * the University Corporation for Advanced Internet Development 
+ * <http://www.ucaid.edu>Internet2 Project. Alternately, this acknowledegement 
+ * may appear in the software itself, if and wherever such third-party 
+ * acknowledgments normally appear.
+ * 
+ * Neither the name of Shibboleth nor the names of its contributors, nor 
+ * Internet2, nor the University Corporation for Advanced Internet Development, 
+ * Inc., nor UCAID may be used to endorse or promote products derived from this 
+ * software without specific prior written permission. For written permission, 
+ * please contact shibboleth@shibboleth.org
+ * 
+ * Products derived from this software may not be called Shibboleth, Internet2, 
+ * UCAID, or the University Corporation for Advanced Internet Development, nor 
+ * may Shibboleth appear in their name, without prior written permission of the 
+ * University Corporation for Advanced Internet Development.
+ * 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND WITH ALL FAULTS. ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+ * PARTICULAR PURPOSE, AND NON-INFRINGEMENT ARE DISCLAIMED AND THE ENTIRE RISK 
+ * OF SATISFACTORY QUALITY, PERFORMANCE, ACCURACY, AND EFFORT IS WITH LICENSEE. 
+ * IN NO EVENT SHALL THE COPYRIGHT OWNER, CONTRIBUTORS OR THE UNIVERSITY 
+ * CORPORATION FOR ADVANCED INTERNET DEVELOPMENT, INC. BE LIABLE FOR ANY DIRECT, 
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package edu.internet2.middleware.shibboleth.wayf;
 
 import javax.servlet.http.Cookie;
@@ -11,7 +60,7 @@ import javax.servlet.http.HttpServletResponse;
  * @author Walter Hoehn wassa&#064;columbia.edu
  */
 public class CookieWayfCache extends WayfCacheBase implements WayfCache {
-	
+
 	private int expiration;
 	private String domain;
 
@@ -21,14 +70,12 @@ public class CookieWayfCache extends WayfCacheBase implements WayfCache {
 	 * @param domain Domain to which the cookie will be released
 	 */
 	public CookieWayfCache(int expiration, String domain) {
-		
-			this.expiration = expiration;
-			if (domain !=null && domain != "") {
-				this.domain = domain;
-			}
+
+		this.expiration = expiration;
+		if (domain != null && domain != "") {
+			this.domain = domain;
+		}
 	}
-
-
 
 	/**
 	 * @see WayfCache#addHsToCache(HttpServletRequest)
@@ -37,7 +84,10 @@ public class CookieWayfCache extends WayfCacheBase implements WayfCache {
 		String handleService,
 		HttpServletRequest req,
 		HttpServletResponse res) {
-		Cookie cacheCookie = new Cookie("edu.internet2.middleware.shibboleth.wayf.selectedHandleService", handleService);
+		Cookie cacheCookie =
+			new Cookie(
+				"edu.internet2.middleware.shibboleth.wayf.selectedHandleService",
+				handleService);
 		configureCookie(cacheCookie);
 		res.addCookie(cacheCookie);
 	}
@@ -51,7 +101,9 @@ public class CookieWayfCache extends WayfCacheBase implements WayfCache {
 
 		Cookie[] cookies = req.getCookies();
 		for (int i = 0; i < cookies.length; i++) {
-			if (cookies[i].getName().equals("edu.internet2.middleware.shibboleth.wayf.selectedHandleService")) {
+			if (cookies[i]
+				.getName()
+				.equals("edu.internet2.middleware.shibboleth.wayf.selectedHandleService")) {
 				configureCookie(cookies[i]);
 				cookies[i].setMaxAge(0);
 				res.addCookie(cookies[i]);
@@ -67,21 +119,23 @@ public class CookieWayfCache extends WayfCacheBase implements WayfCache {
 		Cookie[] cookies = req.getCookies();
 		if (cookies != null) {
 			for (int i = 0; i < cookies.length; i++) {
-				if (cookies[i].getName().equals("edu.internet2.middleware.shibboleth.wayf.selectedHandleService")) {
+				if (cookies[i]
+					.getName()
+					.equals("edu.internet2.middleware.shibboleth.wayf.selectedHandleService")) {
 					return cookies[i].getValue();
 				}
 			}
 		}
 		return null;
 	}
-	
+
 	private void configureCookie(Cookie cookie) {
-		
+
 		cookie.setComment(
 			"Used to cache selection of a user's Shibboleth Handle Service");
 		cookie.setPath("/");
 
-		if (expiration > 0) {    
+		if (expiration > 0) {
 			cookie.setMaxAge(expiration);
 		}
 		if (domain != null && domain != "") {
