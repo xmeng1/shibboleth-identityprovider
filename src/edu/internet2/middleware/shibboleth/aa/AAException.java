6f5465f1560f7214c4ca11d62c6ccfe853ee1784
wassa <wassa@ab3bd59b-922f-494d-bb5f-6f0a3c29deca>
2002-10-18 19:26:51 +0000

    AAException methods didn't work because the message was not being passed up.
    git-svn-id: https://subversion.switch.ch/svn/shibboleth/java-idp/trunk@322 ab3bd59b-922f-494d-bb5f-6f0a3c29deca

--- src/edu/internet2/middleware/shibboleth/aa/AAException.java
+++ src/edu/internet2/middleware/shibboleth/aa/AAException.java
@@ -57,13 +57,8 @@ package edu.internet2.middleware.shibboleth.aa;
  * @created    June, 2002
  */
 
-
-public class AAException extends Exception{
-    String msg;
-    public AAException(String s){
-	msg = s;
-    }
-    public String toString(){
-	return msg;
-    }
+public class AAException extends Exception {
+	public AAException(String s) {
+		super(s);
+	}
 }
