85cebe323f4d1cbefd0ebe30a60473f0b13f4a08
wassa <wassa@ab3bd59b-922f-494d-bb5f-6f0a3c29deca>
2003-12-11 19:28:54 +0000

    Don't error out when loading incomplete cert chain.  Tests for the same.
    git-svn-id: https://subversion.switch.ch/svn/shibboleth/java-idp/trunk@817 ab3bd59b-922f-494d-bb5f-6f0a3c29deca

--- src/edu/internet2/middleware/shibboleth/common/Credentials.java
+++ src/edu/internet2/middleware/shibboleth/common/Credentials.java
@@ -1,48 +1,38 @@
 /*
- * The Shibboleth License, Version 1. Copyright (c) 2002 University Corporation
- * for Advanced Internet Development, Inc. All rights reserved
+ * The Shibboleth License, Version 1. Copyright (c) 2002 University Corporation for Advanced Internet Development, Inc.
+ * All rights reserved
  * 
  * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
  * 
- * Redistributions of source code must retain the above copyright notice, this
- * list of conditions and the following disclaimer.
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ * disclaimer.
  * 
- * Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution, if any, must include
- * the following acknowledgment: "This product includes software developed by
- * the University Corporation for Advanced Internet Development
- * <http://www.ucaid.edu> Internet2 Project. Alternately, this acknowledegement
- * may appear in the software itself, if and wherever such third-party
- * acknowledgments normally appear.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided with the distribution, if any, must include the
+ * following acknowledgment: "This product includes software developed by the University Corporation for Advanced
+ * Internet Development <http://www.ucaid.edu> Internet2 Project. Alternately, this acknowledegement may appear in the
+ * software itself, if and wherever such third-party acknowledgments normally appear.
  * 
- * Neither the name of Shibboleth nor the names of its contributors, nor
- * Internet2, nor the University Corporation for Advanced Internet Development,
- * Inc., nor UCAID may be used to endorse or promote products derived from this
- * software without specific prior written permission. For written permission,
- * please contact shibboleth@shibboleth.org
+ * Neither the name of Shibboleth nor the names of its contributors, nor Internet2, nor the University Corporation for
+ * Advanced Internet Development, Inc., nor UCAID may be used to endorse or promote products derived from this software
+ * without specific prior written permission. For written permission, please contact shibboleth@shibboleth.org
  * 
- * Products derived from this software may not be called Shibboleth, Internet2,
- * UCAID, or the University Corporation for Advanced Internet Development, nor
- * may Shibboleth appear in their name, without prior written permission of the
+ * Products derived from this software may not be called Shibboleth, Internet2, UCAID, or the University Corporation
+ * for Advanced Internet Development, nor may Shibboleth appear in their name, without prior written permission of the
  * University Corporation for Advanced Internet Development.
  * 
  * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND WITH ALL FAULTS. ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
- * PARTICULAR PURPOSE, AND NON-INFRINGEMENT ARE DISCLAIMED AND THE ENTIRE RISK
- * OF SATISFACTORY QUALITY, PERFORMANCE, ACCURACY, AND EFFORT IS WITH LICENSEE.
- * IN NO EVENT SHALL THE COPYRIGHT OWNER, CONTRIBUTORS OR THE UNIVERSITY
- * CORPORATION FOR ADVANCED INTERNET DEVELOPMENT, INC. BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND WITH ALL FAULTS. ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, AND NON-INFRINGEMENT ARE DISCLAIMED AND THE ENTIRE RISK OF SATISFACTORY QUALITY, PERFORMANCE,
+ * ACCURACY, AND EFFORT IS WITH LICENSEE. IN NO EVENT SHALL THE COPYRIGHT OWNER, CONTRIBUTORS OR THE UNIVERSITY
+ * CORPORATION FOR ADVANCED INTERNET DEVELOPMENT, INC. BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 package edu.internet2.middleware.shibboleth.common;
@@ -218,7 +208,6 @@ class FileCredentialResolver implements CredentialResolver {
 		log.debug("Key Format: (" + keyFormat + ").");
 		log.debug("Key Path: (" + keyPath + ").");
 
-		//TODO encrypted keys
 		//TODO maybe more info statements
 
 		PrivateKey key = null;
@@ -325,7 +314,6 @@ class FileCredentialResolver implements CredentialResolver {
 				}
 			}
 
-			//TODO probably don't want to require a full chain
 			log.debug("Attempting to construct a certificate chain.");
 			walkChain((X509Certificate[]) allCerts.toArray(new X509Certificate[0]), certChain);
 
@@ -1139,11 +1127,9 @@ class FileCredentialResolver implements CredentialResolver {
 
 	/**
 	 * 
-	 * Loads a specified bundle of certs individually and returns an array of
-	 * <code>Certificate</code> objects. This is needed because the standard
-	 * <code>CertificateFactory.getCertificates(InputStream)</code> method
-	 * bails out when it has trouble loading any cert and cannot handle
-	 * "comments".
+	 * Loads a specified bundle of certs individually and returns an array of <code>Certificate</code> objects. This
+	 * is needed because the standard <code>CertificateFactory.getCertificates(InputStream)</code> method bails out
+	 * when it has trouble loading any cert and cannot handle "comments".
 	 */
 	private Certificate[] loadCertificates(InputStream inStream, String certType) throws CredentialFactoryException {
 
@@ -1199,17 +1185,15 @@ class FileCredentialResolver implements CredentialResolver {
 	}
 
 	/**
-	 * Given an ArrayList containing a base certificate and an array of
-	 * unordered certificates, populates the ArrayList with an ordered
-	 * certificate chain, based on subject and issuer.
+	 * Given an ArrayList containing a base certificate and an array of unordered certificates, populates the ArrayList
+	 * with an ordered certificate chain, based on subject and issuer.
 	 * 
 	 * @param chainSource
 	 *            array of certificates to pull from
 	 * @param chainDest
 	 *            ArrayList containing base certificate
 	 * @throws InvalidCertificateChainException
-	 *             thrown if a chain cannot be constructed from the specified
-	 *             elements
+	 *             thrown if a chain cannot be constructed from the specified elements
 	 */
 
 	protected void walkChain(X509Certificate[] chainSource, ArrayList chainDest) throws CredentialFactoryException {
@@ -1219,7 +1203,6 @@ class FileCredentialResolver implements CredentialResolver {
 			log.debug("Found self-signed root cert: " + currentCert.getSubjectDN());
 			return;
 		} else {
-			//TODO maybe this should check more than the DN...
 			for (int i = 0; chainSource.length > i; i++) {
 				if (currentCert.getIssuerDN().equals(chainSource[i].getSubjectDN())) {
 					chainDest.add(chainSource[i]);
@@ -1227,14 +1210,12 @@ class FileCredentialResolver implements CredentialResolver {
 					return;
 				}
 			}
-			log.error("Incomplete certificate chain.");
-			throw new CredentialFactoryException("Incomplete cerficate chain.");
+			log.debug("Certificate chain is incomplete.");
 		}
 	}
 
 	/**
-	 * Boolean indication of whether a given private key and public key form a
-	 * valid keypair.
+	 * Boolean indication of whether a given private key and public key form a valid keypair.
 	 * 
 	 * @param pubKey
 	 *            the public key
--- tests/edu/internet2/middleware/shibboleth/common/CredentialsTests.java
+++ tests/edu/internet2/middleware/shibboleth/common/CredentialsTests.java
@@ -1,48 +1,38 @@
 /*
- * The Shibboleth License, Version 1. Copyright (c) 2002 University Corporation
- * for Advanced Internet Development, Inc. All rights reserved
+ * The Shibboleth License, Version 1. Copyright (c) 2002 University Corporation for Advanced Internet Development, Inc.
+ * All rights reserved
  * 
  * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
  * 
- * Redistributions of source code must retain the above copyright notice, this
- * list of conditions and the following disclaimer.
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ * disclaimer.
  * 
- * Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution, if any, must include
- * the following acknowledgment: "This product includes software developed by
- * the University Corporation for Advanced Internet Development
- * <http://www.ucaid.edu> Internet2 Project. Alternately, this acknowledegement
- * may appear in the software itself, if and wherever such third-party
- * acknowledgments normally appear.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided with the distribution, if any, must include the
+ * following acknowledgment: "This product includes software developed by the University Corporation for Advanced
+ * Internet Development <http://www.ucaid.edu> Internet2 Project. Alternately, this acknowledegement may appear in the
+ * software itself, if and wherever such third-party acknowledgments normally appear.
  * 
- * Neither the name of Shibboleth nor the names of its contributors, nor
- * Internet2, nor the University Corporation for Advanced Internet Development,
- * Inc., nor UCAID may be used to endorse or promote products derived from this
- * software without specific prior written permission. For written permission,
- * please contact shibboleth@shibboleth.org
+ * Neither the name of Shibboleth nor the names of its contributors, nor Internet2, nor the University Corporation for
+ * Advanced Internet Development, Inc., nor UCAID may be used to endorse or promote products derived from this software
+ * without specific prior written permission. For written permission, please contact shibboleth@shibboleth.org
  * 
- * Products derived from this software may not be called Shibboleth, Internet2,
- * UCAID, or the University Corporation for Advanced Internet Development, nor
- * may Shibboleth appear in their name, without prior written permission of the
+ * Products derived from this software may not be called Shibboleth, Internet2, UCAID, or the University Corporation
+ * for Advanced Internet Development, nor may Shibboleth appear in their name, without prior written permission of the
  * University Corporation for Advanced Internet Development.
  * 
  * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND WITH ALL FAULTS. ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
- * PARTICULAR PURPOSE, AND NON-INFRINGEMENT ARE DISCLAIMED AND THE ENTIRE RISK
- * OF SATISFACTORY QUALITY, PERFORMANCE, ACCURACY, AND EFFORT IS WITH LICENSEE.
- * IN NO EVENT SHALL THE COPYRIGHT OWNER, CONTRIBUTORS OR THE UNIVERSITY
- * CORPORATION FOR ADVANCED INTERNET DEVELOPMENT, INC. BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND WITH ALL FAULTS. ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, AND NON-INFRINGEMENT ARE DISCLAIMED AND THE ENTIRE RISK OF SATISFACTORY QUALITY, PERFORMANCE,
+ * ACCURACY, AND EFFORT IS WITH LICENSEE. IN NO EVENT SHALL THE COPYRIGHT OWNER, CONTRIBUTORS OR THE UNIVERSITY
+ * CORPORATION FOR ADVANCED INTERNET DEVELOPMENT, INC. BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 package edu.internet2.middleware.shibboleth.common;
@@ -94,7 +84,6 @@ public class CredentialsTests extends TestCase {
 	protected void setUp() throws Exception {
 		super.setUp();
 		try {
-			//TODO turn this back on when you get the schema worked out
 			parser.setFeature("http://xml.org/sax/features/validation", true);
 			parser.setFeature("http://apache.org/xml/features/validation/schema", true);
 			parser.setEntityResolver(new EntityResolver() {
@@ -145,7 +134,7 @@ public class CredentialsTests extends TestCase {
 
 	}
 
-	public void testKeyStoreX509() {
+	public void testKeyStoreX509CompleteChain() {
 
 		try {
 			InputStream inStream = new FileInputStream("data/credentials1.xml");
@@ -169,8 +158,70 @@ public class CredentialsTests extends TestCase {
 				new Integer(3));
 			assertEquals(
 				"Unexpected X509 certificate found.",
+				credential.getX509CertificateChain()[1].getSubjectDN().getName(),
+				"CN=HEPKI Server CA -- 20020701A, OU=Division of Information Technology, O=University of Wisconsin, L=Madison, ST=Wisconsin, C=US");
+			assertEquals(
+				"Unexpected X509 certificate found.",
 				credential.getX509CertificateChain()[2].getSubjectDN().getName(),
 				"CN=HEPKI Master CA -- 20020701A, OU=Division of Information Technology, O=University of Wisconsin, L=Madison, ST=Wisconsin, C=US");
+		} catch (Exception e) {
+			fail("Failed to load credentials: " + e);
+		}
+	}
+
+	public void testKeyStoreX509EndOnly() {
+
+		try {
+			InputStream inStream = new FileInputStream("data/credentials16.xml");
+			parser.parse(new InputSource(inStream));
+			Credentials credentials = new Credentials(parser.getDocument().getDocumentElement());
+
+			assertTrue("Credential could not be found.", credentials.containsCredential("test"));
+			Credential credential = credentials.getCredential("test");
+
+			assertTrue(
+				"Credential was loaded with an incorrect type.",
+				credential.getCredentialType() == Credential.X509);
+			assertNotNull("Private key was not loaded correctly.", credential.getPrivateKey());
+			assertEquals(
+				"Unexpected X509 certificate found.",
+				credential.getX509Certificate().getSubjectDN().getName(),
+				"CN=shib2.internet2.edu, OU=Unknown, O=Unknown, ST=Unknown, C=Unknown");
+			assertEquals(
+				"Unexpected certificate chain length.",
+				new Integer(credential.getX509CertificateChain().length),
+				new Integer(1));
+		} catch (Exception e) {
+			fail("Failed to load credentials: " + e);
+		}
+	}
+
+	public void testKeyStoreX509IncompleteChain() {
+
+		try {
+			InputStream inStream = new FileInputStream("data/credentials17.xml");
+			parser.parse(new InputSource(inStream));
+			Credentials credentials = new Credentials(parser.getDocument().getDocumentElement());
+
+			assertTrue("Credential could not be found.", credentials.containsCredential("test"));
+			Credential credential = credentials.getCredential("test");
+
+			assertTrue(
+				"Credential was loaded with an incorrect type.",
+				credential.getCredentialType() == Credential.X509);
+			assertNotNull("Private key was not loaded correctly.", credential.getPrivateKey());
+			assertEquals(
+				"Unexpected X509 certificate found.",
+				credential.getX509Certificate().getSubjectDN().getName(),
+				"CN=shib2.internet2.edu, OU=Unknown, O=Unknown, ST=Unknown, C=Unknown");
+			assertEquals(
+				"Unexpected certificate chain length.",
+				new Integer(credential.getX509CertificateChain().length),
+				new Integer(2));
+			assertEquals(
+				"Unexpected X509 certificate found.",
+				credential.getX509CertificateChain()[1].getSubjectDN().getName(),
+				"CN=HEPKI Server CA -- 20020701A, OU=Division of Information Technology, O=University of Wisconsin, L=Madison, ST=Wisconsin, C=US");
 		} catch (Exception e) {
 			fail("Failed to load credentials: " + e);
 		}
