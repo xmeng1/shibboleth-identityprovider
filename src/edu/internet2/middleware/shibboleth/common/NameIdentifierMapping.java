b38d302eec0398ba6fe745fcc604bcdb96d0a30c
wassa <wassa@ab3bd59b-922f-494d-bb5f-6f0a3c29deca>
2004-02-05 20:08:54 +0000

    Updated javadoc.
    git-svn-id: https://subversion.switch.ch/svn/shibboleth/java-idp/trunk@886 ab3bd59b-922f-494d-bb5f-6f0a3c29deca

--- src/edu/internet2/middleware/shibboleth/common/NameIdentifierMapping.java
+++ src/edu/internet2/middleware/shibboleth/common/NameIdentifierMapping.java
@@ -1,48 +1,38 @@
 /*
- * The Shibboleth License, Version 1. Copyright (c) 2002 University Corporation
- * for Advanced Internet Development, Inc. All rights reserved
+ * The Shibboleth License, Version 1. Copyright (c) 2002 University Corporation for Advanced Internet Development, Inc.
+ * All rights reserved
  * 
  * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
  * 
- * Redistributions of source code must retain the above copyright notice, this
- * list of conditions and the following disclaimer.
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ * disclaimer.
  * 
- * Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution, if any, must include
- * the following acknowledgment: "This product includes software developed by
- * the University Corporation for Advanced Internet Development
- * <http://www.ucaid.edu> Internet2 Project. Alternately, this acknowledegement
- * may appear in the software itself, if and wherever such third-party
- * acknowledgments normally appear.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided with the distribution, if any, must include the
+ * following acknowledgment: "This product includes software developed by the University Corporation for Advanced
+ * Internet Development <http://www.ucaid.edu> Internet2 Project. Alternately, this acknowledegement may appear in the
+ * software itself, if and wherever such third-party acknowledgments normally appear.
  * 
- * Neither the name of Shibboleth nor the names of its contributors, nor
- * Internet2, nor the University Corporation for Advanced Internet Development,
- * Inc., nor UCAID may be used to endorse or promote products derived from this
- * software without specific prior written permission. For written permission,
- * please contact shibboleth@shibboleth.org
+ * Neither the name of Shibboleth nor the names of its contributors, nor Internet2, nor the University Corporation for
+ * Advanced Internet Development, Inc., nor UCAID may be used to endorse or promote products derived from this software
+ * without specific prior written permission. For written permission, please contact shibboleth@shibboleth.org
  * 
- * Products derived from this software may not be called Shibboleth, Internet2,
- * UCAID, or the University Corporation for Advanced Internet Development, nor
- * may Shibboleth appear in their name, without prior written permission of the
+ * Products derived from this software may not be called Shibboleth, Internet2, UCAID, or the University Corporation
+ * for Advanced Internet Development, nor may Shibboleth appear in their name, without prior written permission of the
  * University Corporation for Advanced Internet Development.
  * 
  * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND WITH ALL FAULTS. ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
- * PARTICULAR PURPOSE, AND NON-INFRINGEMENT ARE DISCLAIMED AND THE ENTIRE RISK
- * OF SATISFACTORY QUALITY, PERFORMANCE, ACCURACY, AND EFFORT IS WITH LICENSEE.
- * IN NO EVENT SHALL THE COPYRIGHT OWNER, CONTRIBUTORS OR THE UNIVERSITY
- * CORPORATION FOR ADVANCED INTERNET DEVELOPMENT, INC. BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND WITH ALL FAULTS. ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, AND NON-INFRINGEMENT ARE DISCLAIMED AND THE ENTIRE RISK OF SATISFACTORY QUALITY, PERFORMANCE,
+ * ACCURACY, AND EFFORT IS WITH LICENSEE. IN NO EVENT SHALL THE COPYRIGHT OWNER, CONTRIBUTORS OR THE UNIVERSITY
+ * CORPORATION FOR ADVANCED INTERNET DEVELOPMENT, INC. BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 package edu.internet2.middleware.shibboleth.common;
@@ -52,8 +42,7 @@ import java.net.URI;
 import org.opensaml.SAMLNameIdentifier;
 
 /**
- * Defines a mechanism for converting SAML Name Identifiers to local <code>AuthNPrincipal</code>
- * objects.
+ * Defines a mechanism for converting SAML Name Identifiers to local {@link AuthNPrincipal}objects.
  * 
  * @author Walter Hoehn
  */
@@ -69,8 +58,7 @@ public interface NameIdentifierMapping {
 	public URI getNameIdentifierFormat();
 
 	/**
-	 * Maps a SAML Name Identifier to a local principal using the appropriate
-	 * registered mapping.
+	 * Maps a SAML Name Identifier to a local principal using the appropriate registered mapping.
 	 * 
 	 * @param nameId
 	 *            the SAML Name Identifier that should be converted
@@ -80,11 +68,9 @@ public interface NameIdentifierMapping {
 	 *            the provider handling the request
 	 * @return the local principal
 	 * @throws NameIdentifierMappingException
-	 *             If the <code>NameMapper</code> encounters an internal
-	 *             error
+	 *             If the {@link NameMapper}encounters an internal error
 	 * @throws InvalidNameIdentifierException
-	 *             If the <code>SAMLNameIdentifier</code> contains invalid
-	 *             data
+	 *             If the {@link SAMLNameIdentifier}contains invalid data
 	 */
 	public AuthNPrincipal getPrincipal(SAMLNameIdentifier nameId, ServiceProvider sProv, IdentityProvider idProv)
 		throws NameIdentifierMappingException, InvalidNameIdentifierException;
--- src/edu/internet2/middleware/shibboleth/common/NameMapper.java
+++ src/edu/internet2/middleware/shibboleth/common/NameMapper.java
@@ -1,48 +1,38 @@
 /*
- * The Shibboleth License, Version 1. Copyright (c) 2002 University Corporation
- * for Advanced Internet Development, Inc. All rights reserved
+ * The Shibboleth License, Version 1. Copyright (c) 2002 University Corporation for Advanced Internet Development, Inc.
+ * All rights reserved
  * 
  * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
  * 
- * Redistributions of source code must retain the above copyright notice, this
- * list of conditions and the following disclaimer.
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ * disclaimer.
  * 
- * Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution, if any, must include
- * the following acknowledgment: "This product includes software developed by
- * the University Corporation for Advanced Internet Development
- * <http://www.ucaid.edu> Internet2 Project. Alternately, this acknowledegement
- * may appear in the software itself, if and wherever such third-party
- * acknowledgments normally appear.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided with the distribution, if any, must include the
+ * following acknowledgment: "This product includes software developed by the University Corporation for Advanced
+ * Internet Development <http://www.ucaid.edu> Internet2 Project. Alternately, this acknowledegement may appear in the
+ * software itself, if and wherever such third-party acknowledgments normally appear.
  * 
- * Neither the name of Shibboleth nor the names of its contributors, nor
- * Internet2, nor the University Corporation for Advanced Internet Development,
- * Inc., nor UCAID may be used to endorse or promote products derived from this
- * software without specific prior written permission. For written permission,
- * please contact shibboleth@shibboleth.org
+ * Neither the name of Shibboleth nor the names of its contributors, nor Internet2, nor the University Corporation for
+ * Advanced Internet Development, Inc., nor UCAID may be used to endorse or promote products derived from this software
+ * without specific prior written permission. For written permission, please contact shibboleth@shibboleth.org
  * 
- * Products derived from this software may not be called Shibboleth, Internet2,
- * UCAID, or the University Corporation for Advanced Internet Development, nor
- * may Shibboleth appear in their name, without prior written permission of the
+ * Products derived from this software may not be called Shibboleth, Internet2, UCAID, or the University Corporation
+ * for Advanced Internet Development, nor may Shibboleth appear in their name, without prior written permission of the
  * University Corporation for Advanced Internet Development.
  * 
  * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND WITH ALL FAULTS. ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
- * PARTICULAR PURPOSE, AND NON-INFRINGEMENT ARE DISCLAIMED AND THE ENTIRE RISK
- * OF SATISFACTORY QUALITY, PERFORMANCE, ACCURACY, AND EFFORT IS WITH LICENSEE.
- * IN NO EVENT SHALL THE COPYRIGHT OWNER, CONTRIBUTORS OR THE UNIVERSITY
- * CORPORATION FOR ADVANCED INTERNET DEVELOPMENT, INC. BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND WITH ALL FAULTS. ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE, AND NON-INFRINGEMENT ARE DISCLAIMED AND THE ENTIRE RISK OF SATISFACTORY QUALITY, PERFORMANCE,
+ * ACCURACY, AND EFFORT IS WITH LICENSEE. IN NO EVENT SHALL THE COPYRIGHT OWNER, CONTRIBUTORS OR THE UNIVERSITY
+ * CORPORATION FOR ADVANCED INTERNET DEVELOPMENT, INC. BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 package edu.internet2.middleware.shibboleth.common;
@@ -64,8 +54,8 @@ import org.xml.sax.InputSource;
 import edu.internet2.middleware.shibboleth.hs.provider.SharedMemoryShibHandle;
 
 /**
- * Facility for managing mappings from SAML Name Identifiers to local <code>AuthNPrincipal</code>
- * objects. Mappings are registered by Name Identifier format.
+ * Facility for managing mappings from SAML Name Identifiers to local {@link AuthNPrincipal}objects. Mappings are
+ * registered by Name Identifier format.
  * 
  * @author Walter Hoehn
  * @see NameIdentifierMapping
@@ -100,6 +90,9 @@ public class NameMapper {
 		}
 	}
 
+	/**
+	 * Constructs name mapper and loads a default name mapping.
+	 */
 	public NameMapper() {
 		try {
 			//Load the default mapping
@@ -123,24 +116,21 @@ public class NameMapper {
 	}
 
 	/**
-	 * 
-	 * Constructs a <code>NameIdentifierMapping</code> based on XML
-	 * configuration data and adds it to this <code>NameMapper</code>,
-	 * registering it according to its format.
+	 * Constructs a {@link NameIdentifierMapping}based on XML configuration data and adds it to this
+	 * {@link NameMapper}, registering it according to its format.
 	 * 
 	 * @param e
-	 *            An XML representation of a <code>NameIdentifierMapping</code>
+	 *            An XML representation of a {@link NameIdentifierMapping}
 	 * 
 	 * @throws NameIdentifierMappingException
-	 *             If the mapping could not be constructed according to the
-	 *             supplied configuration
+	 *             If the mapping could not be constructed according to the supplied configuration
 	 */
 	public void addNameMapping(Element e) throws NameIdentifierMappingException {
-		
+
 		if (!e.getLocalName().equals("NameMapping")) {
 			throw new IllegalArgumentException();
 		}
-		
+
 		log.info("Found Name Mapping. Loading...");
 
 		String type = ((Element) e).getAttribute("type");
@@ -184,8 +174,8 @@ public class NameMapper {
 	}
 
 	/**
-	 * Adds a <code>NameIdentifierMapping</code> to this <code>NameMapper</code>,
-	 * registering it according to its format.
+	 * Adds a <code>NameIdentifierMapping</code> to this <code>NameMapper</code>, registering it according to its
+	 * format.
 	 * 
 	 * @param mapping
 	 *            the mapping to add
@@ -203,13 +193,11 @@ public class NameMapper {
 	}
 
 	/**
-	 * Returns the <code>NameIdentifierMapping</code> registered for a given
-	 * format
+	 * Returns the <code>NameIdentifierMapping</code> registered for a given format
 	 * 
 	 * @param format
 	 *            the registered format
-	 * @return the mapping or <tt>null</tt> if no mapping is registered for
-	 *         the given format
+	 * @return the mapping or <tt>null</tt> if no mapping is registered for the given format
 	 */
 	public NameIdentifierMapping getNameIdentifierMapping(URI format) {
 		if (!initialized) {
@@ -244,8 +232,7 @@ public class NameMapper {
 	}
 
 	/**
-	 * Maps a SAML Name Identifier to a local principal using the appropriate
-	 * registered mapping.
+	 * Maps a SAML Name Identifier to a local principal using the appropriate registered mapping.
 	 * 
 	 * @param nameId
 	 *            the SAML Name Identifier that should be converted
@@ -255,11 +242,9 @@ public class NameMapper {
 	 *            the provider handling the request
 	 * @return the local principal
 	 * @throws NameIdentifierMappingException
-	 *             If the <code>NameMapper</code> encounters an internal
-	 *             error
+	 *             If the <code>NameMapper</code> encounters an internal error
 	 * @throws InvalidNameIdentifierException
-	 *             If the <code>SAMLNameIdentifier</code> contains invalid
-	 *             data
+	 *             If the <code>SAMLNameIdentifier</code> contains invalid data
 	 */
 	public AuthNPrincipal getPrincipal(SAMLNameIdentifier nameId, ServiceProvider sProv, IdentityProvider idProv)
 		throws NameIdentifierMappingException, InvalidNameIdentifierException {
