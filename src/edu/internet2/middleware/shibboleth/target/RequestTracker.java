2340670af923a431d6b2b25b73de2f93644f5d0b
gilbert <gilbert@ab3bd59b-922f-494d-bb5f-6f0a3c29deca>
2004-10-28 13:25:46 +0000

    Prototype thread local (per request) object. May prove useless and be removed.
    git-svn-id: https://subversion.switch.ch/svn/shibboleth/java-idp/trunk@1151 ab3bd59b-922f-494d-bb5f-6f0a3c29deca

---
+++ src/edu/internet2/middleware/shibboleth/target/RequestTracker.java
@@ -0,0 +1,37 @@
+/*
+ * RequestTracker.java
+ * 
+ * An object representing a sanitized version of information
+ * on the current request. Typically, data will be extracted 
+ * from the current HttpRequest and stored here. A reference to 
+ * the object is stored in a ThreadLocal field of the 
+ * ServiceProviderContext when a request arrives, and then the 
+ * reference is nulled before returning to the container.
+ * 
+ * Thread local storage is somewhat expensive, though it gets
+ * more efficient with each release. Therefore, a reference to
+ * this object should be obtained once when needed and saved
+ * in a local variable. Obviously, a reference must never be
+ * saved in a field because the data here is thread-specific.
+ */
+package edu.internet2.middleware.shibboleth.target;
+
+/**
+ * Hold information about the current request in a ThreadLocal object.
+ * 
+ * <p>ServiceProviderContext context = ServiceProviderContext.getInstance();<br />
+ * RequestTracker requestTracker = context.getRequestContext();</p>
+ * 
+ * @author Howard Gilbert
+ */
+public class RequestTracker {
+    
+    String ipaddr = null;
+
+    public String getIpaddr() {
+        return ipaddr;
+    }
+    public void setIpaddr(String ipaddr) {
+        this.ipaddr = ipaddr;
+    }
+}
