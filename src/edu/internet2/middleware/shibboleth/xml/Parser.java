553a90b29f83c005b92ab38ea8d7d63e0d053adf
cantor <cantor@ab3bd59b-922f-494d-bb5f-6f0a3c29deca>
2005-05-05 03:50:50 +0000

    Fixed order of schemas.
    git-svn-id: https://subversion.switch.ch/svn/shibboleth/java-idp/trunk@1441 ab3bd59b-922f-494d-bb5f-6f0a3c29deca

--- src/edu/internet2/middleware/shibboleth/xml/Parser.java
+++ src/edu/internet2/middleware/shibboleth/xml/Parser.java
@@ -71,12 +71,12 @@ public class Parser {
             "urn:mace:shibboleth:idp:config:1.0",
             "urn:mace:shibboleth:arp:1.0",
             "urn:mace:shibboleth:resolver:1.0",
+            "urn:oasis:names:tc:SAML:2.0:metadata",
             "urn:mace:shibboleth:target:config:1.0",
             "urn:mace:shibboleth:trust:1.0",
             "urn:mace:shibboleth:metadata:1.0",
             "urn:mace:shibboleth:1.0",
-            "http://schemas.xmlsoap.org/soap/envelope/",
-            "urn:oasis:names:tc:SAML:2.0:metadata"
+            "http://schemas.xmlsoap.org/soap/envelope/"
       };
     
     // If there were a real Framework here (like Spring) then
