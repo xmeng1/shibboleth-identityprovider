4719c5e345eb0ffb3eb1cc36cadebb4c204bdc1a
wassa <wassa@ab3bd59b-922f-494d-bb5f-6f0a3c29deca>
2005-05-13 14:23:19 +0000

    Metadata provider now supports inline metadata specification.
    git-svn-id: https://subversion.switch.ch/svn/shibboleth/java-idp/trunk@1476 ab3bd59b-922f-494d-bb5f-6f0a3c29deca

--- src/edu/internet2/middleware/shibboleth/metadata/provider/XMLMetadata.java
+++ src/edu/internet2/middleware/shibboleth/metadata/provider/XMLMetadata.java
@@ -2,26 +2,25 @@
  * The Shibboleth License, Version 1. Copyright (c) 2002 University Corporation for Advanced Internet Development, Inc.
  * All rights reserved Redistribution and use in source and binary forms, with or without modification, are permitted
  * provided that the following conditions are met: Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer. Redistributions in binary form must reproduce the
- * above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other
- * materials provided with the distribution, if any, must include the following acknowledgment: "This product includes
- * software developed by the University Corporation for Advanced Internet Development <http://www.ucaid.edu>Internet2
- * Project. Alternately, this acknowledegement may appear in the software itself, if and wherever such third-party
- * acknowledgments normally appear. Neither the name of Shibboleth nor the names of its contributors, nor Internet2,
- * nor the University Corporation for Advanced Internet Development, Inc., nor UCAID may be used to endorse or promote
- * products derived from this software without specific prior written permission. For written permission, please
- * contact shibboleth@shibboleth.org Products derived from this software may not be called Shibboleth, Internet2,
- * UCAID, or the University Corporation for Advanced Internet Development, nor may Shibboleth appear in their name,
- * without prior written permission of the University Corporation for Advanced Internet Development. THIS SOFTWARE IS
- * PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND WITH ALL FAULTS. ANY EXPRESS OR IMPLIED WARRANTIES,
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND
- * NON-INFRINGEMENT ARE DISCLAIMED AND THE ENTIRE RISK OF SATISFACTORY QUALITY, PERFORMANCE, ACCURACY, AND EFFORT IS
- * WITH LICENSEE. IN NO EVENT SHALL THE COPYRIGHT OWNER, CONTRIBUTORS OR THE UNIVERSITY CORPORATION FOR ADVANCED
- * INTERNET DEVELOPMENT, INC. BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
- * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ * notice, this list of conditions and the following disclaimer. Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials
+ * provided with the distribution, if any, must include the following acknowledgment: "This product includes software
+ * developed by the University Corporation for Advanced Internet Development <http://www.ucaid.edu>Internet2 Project.
+ * Alternately, this acknowledegement may appear in the software itself, if and wherever such third-party
+ * acknowledgments normally appear. Neither the name of Shibboleth nor the names of its contributors, nor Internet2, nor
+ * the University Corporation for Advanced Internet Development, Inc., nor UCAID may be used to endorse or promote
+ * products derived from this software without specific prior written permission. For written permission, please contact
+ * shibboleth@shibboleth.org Products derived from this software may not be called Shibboleth, Internet2, UCAID, or the
+ * University Corporation for Advanced Internet Development, nor may Shibboleth appear in their name, without prior
+ * written permission of the University Corporation for Advanced Internet Development. THIS SOFTWARE IS PROVIDED BY THE
+ * COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND WITH ALL FAULTS. ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT ARE
+ * DISCLAIMED AND THE ENTIRE RISK OF SATISFACTORY QUALITY, PERFORMANCE, ACCURACY, AND EFFORT IS WITH LICENSEE. IN NO
+ * EVENT SHALL THE COPYRIGHT OWNER, CONTRIBUTORS OR THE UNIVERSITY CORPORATION FOR ADVANCED INTERNET DEVELOPMENT, INC.
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 package edu.internet2.middleware.shibboleth.metadata.provider;
@@ -34,6 +33,7 @@ import org.opensaml.XML;
 import org.opensaml.artifact.Artifact;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
@@ -51,70 +51,115 @@ import edu.internet2.middleware.shibboleth.xml.Parser;
  */
 public class XMLMetadata extends ResourceWatchdog implements Metadata {
 
-	private static Logger	log	= Logger.getLogger(XMLMetadataLoadWrapper.class.getName());
-	private Metadata		currentMeta;
+	private static Logger log = Logger.getLogger(XMLMetadataLoadWrapper.class.getName());
+	private Metadata currentMeta;
 
 	public XMLMetadata(Element configuration) throws MetadataException, ResourceNotAvailableException {
-		this(configuration.getAttribute("uri"));
+
+		super(new ShibResource(configuration.getAttribute("uri"), XMLMetadata.class));
+
+		try {
+
+			if (configuration.getAttribute("uri") != null && !configuration.getAttribute("uri").equals("")) {
+				// The configuration points to a metadata file
+				InputSource src = new InputSource(resource.getInputStream());
+				src.setSystemId(resource.getURL().toString());
+				Document doc = Parser.loadDom(src, true);
+				currentMeta = new XMLMetadataProvider(doc.getDocumentElement());
+
+				// Start checking for metadata updates
+				start();
+
+			} else {
+				// Hopefully, the configuration is inline (don't reload in this case)
+				NodeList children = configuration.getChildNodes();
+				for (int i = 0; i < children.getLength(); i++) {
+
+					if ((children.item(i) instanceof Element)
+							&& (XML.isElementNamed((Element) children.item(i),
+									edu.internet2.middleware.shibboleth.common.XML.SAML2META_NS, "EntitiesDescriptor") || XML
+									.isElementNamed((Element) children.item(i),
+											edu.internet2.middleware.shibboleth.common.XML.SAML2META_NS,
+											"EntityDescriptor"))) {
+						currentMeta = new XMLMetadataProvider((Element) children.item(i));
+						return;
+					}
+				}
+				// We didn't find a uri pointer or inline metadata, bail out
+				log.error("Encountered a problem reading metadata: <MetadataProvider/> configuration must "
+						+ "include either a (uri) attribute or inline metadata.");
+				throw new MetadataException("Unable to read metadata.");
+			}
+
+		} catch (IOException e) {
+			log.error("Encountered a problem reading metadata source: " + e);
+			throw new MetadataException("Unable to read metadata: " + e);
+		} catch (SAXException e) {
+			log.error("Encountered a problem parsing metadata source: " + e);
+			throw new MetadataException("Unable to read metadata: " + e);
+		} catch (SAMLException e) {
+			log.error("Encountered a problem processing metadata source: " + e);
+			throw new MetadataException("Unable to read metadata: + e");
+		}
+
 	}
 
 	public XMLMetadata(String sitesFileLocation) throws MetadataException, ResourceNotAvailableException {
+
 		super(new ShibResource(sitesFileLocation, XMLMetadata.class));
 		try {
-            InputSource src = new InputSource(resource.getInputStream());
-            src.setSystemId(resource.getURL().toString());
-			Document doc = Parser.loadDom(src,true);
+			InputSource src = new InputSource(resource.getInputStream());
+			src.setSystemId(resource.getURL().toString());
+			Document doc = Parser.loadDom(src, true);
 			currentMeta = new XMLMetadataProvider(doc.getDocumentElement());
 		} catch (IOException e) {
 			log.error("Encountered a problem reading metadata source: " + e);
 			throw new MetadataException("Unable to read metadata: " + e);
+		} catch (SAXException e) {
+			log.error("Encountered a problem parsing metadata source: " + e);
+			throw new MetadataException("Unable to read metadata: " + e);
+		} catch (SAMLException e) {
+			log.error("Encountered a problem processing metadata source: " + e);
+			throw new MetadataException("Unable to read metadata: + e");
 		}
-        catch (SAXException e) {
-            log.error("Encountered a problem parsing metadata source: " + e);
-            throw new MetadataException("Unable to read metadata: " + e);
-        }
-        catch (SAMLException e) {
-            log.error("Encountered a problem processing metadata source: " + e);
-            throw new MetadataException("Unable to read metadata: + e");
-        }
 
-		//Start checking for metadata updates
+		// Start checking for metadata updates
 		start();
 
 	}
 
 	public EntityDescriptor lookup(String providerId) {
+
 		synchronized (currentMeta) {
 			return currentMeta.lookup(providerId);
 		}
 	}
 
-    public EntityDescriptor lookup(Artifact artifact) {
-        synchronized (currentMeta) {
-            return currentMeta.lookup(artifact);
-        }
-    }
-    
+	public EntityDescriptor lookup(Artifact artifact) {
+
+		synchronized (currentMeta) {
+			return currentMeta.lookup(artifact);
+		}
+	}
+
 	protected void doOnChange() throws ResourceWatchdogExecutionException {
-        Metadata newMeta = null;
-        Document newDoc = null;
+
+		Metadata newMeta = null;
+		Document newDoc = null;
 
 		try {
 			log.info("Detected a change in the metadata. Reloading from (" + resource.getURL().toString() + ").");
-            newMeta = new XMLMetadataProvider(XML.parserPool.parse(resource.getInputStream()).getDocumentElement());
-        }
-        catch (IOException e) {
+			newMeta = new XMLMetadataProvider(XML.parserPool.parse(resource.getInputStream()).getDocumentElement());
+		} catch (IOException e) {
+			log.error("Encountered an error retrieving updated SAML metadata, continuing to use stale copy: " + e);
+			return;
+		} catch (SAXException e) {
+			log.error("Encountered an error retrieving updated SAML metadata, continuing to use stale copy: " + e);
+			return;
+		} catch (SAMLException e) {
 			log.error("Encountered an error retrieving updated SAML metadata, continuing to use stale copy: " + e);
 			return;
 		}
-        catch (SAXException e) {
-            log.error("Encountered an error retrieving updated SAML metadata, continuing to use stale copy: " + e);
-            return;
-        }
-        catch (SAMLException e) {
-            log.error("Encountered an error retrieving updated SAML metadata, continuing to use stale copy: " + e);
-            return;
-        }
 
 		if (newMeta != null) {
 			synchronized (currentMeta) {
--- tests/edu/internet2/middleware/shibboleth/metadata/MetadataTests.java
+++ tests/edu/internet2/middleware/shibboleth/metadata/MetadataTests.java
@@ -2,33 +2,35 @@
  * The Shibboleth License, Version 1. Copyright (c) 2002 University Corporation for Advanced Internet Development, Inc.
  * All rights reserved Redistribution and use in source and binary forms, with or without modification, are permitted
  * provided that the following conditions are met: Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer. Redistributions in binary form must reproduce the
- * above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other
- * materials provided with the distribution, if any, must include the following acknowledgment: "This product includes
- * software developed by the University Corporation for Advanced Internet Development <http://www.ucaid.edu> Internet2
- * Project. Alternately, this acknowledegement may appear in the software itself, if and wherever such third-party
- * acknowledgments normally appear. Neither the name of Shibboleth nor the names of its contributors, nor Internet2,
- * nor the University Corporation for Advanced Internet Development, Inc., nor UCAID may be used to endorse or promote
- * products derived from this software without specific prior written permission. For written permission, please
- * contact shibboleth@shibboleth.org Products derived from this software may not be called Shibboleth, Internet2,
- * UCAID, or the University Corporation for Advanced Internet Development, nor may Shibboleth appear in their name,
- * without prior written permission of the University Corporation for Advanced Internet Development. THIS SOFTWARE IS
- * PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND WITH ALL FAULTS. ANY EXPRESS OR IMPLIED WARRANTIES,
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND
- * NON-INFRINGEMENT ARE DISCLAIMED AND THE ENTIRE RISK OF SATISFACTORY QUALITY, PERFORMANCE, ACCURACY, AND EFFORT IS
- * WITH LICENSEE. IN NO EVENT SHALL THE COPYRIGHT OWNER, CONTRIBUTORS OR THE UNIVERSITY CORPORATION FOR ADVANCED
- * INTERNET DEVELOPMENT, INC. BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
- * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ * notice, this list of conditions and the following disclaimer. Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials
+ * provided with the distribution, if any, must include the following acknowledgment: "This product includes software
+ * developed by the University Corporation for Advanced Internet Development <http://www.ucaid.edu> Internet2 Project.
+ * Alternately, this acknowledegement may appear in the software itself, if and wherever such third-party
+ * acknowledgments normally appear. Neither the name of Shibboleth nor the names of its contributors, nor Internet2, nor
+ * the University Corporation for Advanced Internet Development, Inc., nor UCAID may be used to endorse or promote
+ * products derived from this software without specific prior written permission. For written permission, please contact
+ * shibboleth@shibboleth.org Products derived from this software may not be called Shibboleth, Internet2, UCAID, or the
+ * University Corporation for Advanced Internet Development, nor may Shibboleth appear in their name, without prior
+ * written permission of the University Corporation for Advanced Internet Development. THIS SOFTWARE IS PROVIDED BY THE
+ * COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND WITH ALL FAULTS. ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT ARE
+ * DISCLAIMED AND THE ENTIRE RISK OF SATISFACTORY QUALITY, PERFORMANCE, ACCURACY, AND EFFORT IS WITH LICENSEE. IN NO
+ * EVENT SHALL THE COPYRIGHT OWNER, CONTRIBUTORS OR THE UNIVERSITY CORPORATION FOR ADVANCED INTERNET DEVELOPMENT, INC.
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 package edu.internet2.middleware.shibboleth.metadata;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.util.Arrays;
 import java.util.Iterator;
+
+import javax.xml.parsers.DocumentBuilderFactory;
 
 import junit.framework.TestCase;
 
@@ -36,8 +38,14 @@ import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.opensaml.XML;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.xml.sax.InputSource;
 
+import edu.internet2.middleware.shibboleth.idp.IdPConfig;
 import edu.internet2.middleware.shibboleth.metadata.provider.XMLMetadata;
+import edu.internet2.middleware.shibboleth.xml.Parser;
 
 /**
  * Validation suite for the <code>Metadata</code> interface.
@@ -47,8 +55,10 @@ import edu.internet2.middleware.shibboleth.metadata.provider.XMLMetadata;
 
 public class MetadataTests extends TestCase {
 
+	private Parser.DOMParser parser = new Parser.DOMParser(true);
 
 	public MetadataTests(String name) {
+
 		super(name);
 		BasicConfigurator.resetConfiguration();
 		BasicConfigurator.configure();
@@ -56,12 +66,14 @@ public class MetadataTests extends TestCase {
 	}
 
 	public static void main(String[] args) {
+
 		junit.textui.TestRunner.run(MetadataTests.class);
 		BasicConfigurator.configure();
 		Logger.getRootLogger().setLevel(Level.OFF);
 	}
 
 	protected void setUp() throws Exception {
+
 		super.setUp();
 
 	}
@@ -74,31 +86,25 @@ public class MetadataTests extends TestCase {
 			assertNotNull("Unable to find test provider", metadata.lookup("bahsite"));
 			assertNotNull("Unable to find test provider", metadata.lookup("rootsite"));
 
-            /* TODO: rework to walk tree
-			assertTrue("Group list is incorrect or out of order.", Arrays.equals(new String[]{"urn:mace:inqueue",
-					"foofed", "bahfed"}, metadata.lookup("bahsite").getGroups()));
-					*/
-            
-			//This should probably be made more robust at some point
-			assertNotNull("Incorrect provider role.", metadata.lookup("bahsite").getSPSSODescriptor(XML.SAML11_PROTOCOL_ENUM));
+			// This should probably be made more robust at some point
+			assertNotNull("Incorrect provider role.", metadata.lookup("bahsite").getSPSSODescriptor(
+					XML.SAML11_PROTOCOL_ENUM));
 
-			assertEquals("Incorrect parsing of assertion consumer URL.",
-                    ((Endpoint)metadata.lookup("bahsite").getSPSSODescriptor(XML.SAML11_PROTOCOL_ENUM).getAssertionConsumerServiceManager().getEndpoints().next()).getLocation(),
-                    "http://foo.com/SHIRE"
-                    );
+			assertEquals("Incorrect parsing of assertion consumer URL.", ((Endpoint) metadata.lookup("bahsite")
+					.getSPSSODescriptor(XML.SAML11_PROTOCOL_ENUM).getAssertionConsumerServiceManager().getEndpoints()
+					.next()).getLocation(), "http://foo.com/SHIRE");
 
-            Iterator keys = metadata.lookup("rootsite").getSPSSODescriptor(XML.SAML11_PROTOCOL_ENUM).getKeyDescriptors();
-            KeyDescriptor key1 = (KeyDescriptor)keys.next();
-            KeyDescriptor key2 = (KeyDescriptor)keys.next();
+			Iterator keys = metadata.lookup("rootsite").getSPSSODescriptor(XML.SAML11_PROTOCOL_ENUM)
+					.getKeyDescriptors();
+			KeyDescriptor key1 = (KeyDescriptor) keys.next();
+			KeyDescriptor key2 = (KeyDescriptor) keys.next();
 			assertTrue("Incorrect attribute requester key parsing.", key1 != null && key2 != null);
 
 			String[] control = new String[]{
 					"C=US, ST=Tennessee, L=Memphis, O=The University of Memphis, OU=Information Systems, CN=test2.memphis.edu",
 					"C=US, ST=Tennessee, L=Memphis, O=The University of Memphis, OU=Information Systems, CN=test1.memphis.edu"};
-			String[] meta = new String[] {
-					key1.getKeyInfo().itemKeyName(0).getKeyName(),
-					key2.getKeyInfo().itemKeyName(0).getKeyName()
-                    };
+			String[] meta = new String[]{key1.getKeyInfo().itemKeyName(0).getKeyName(),
+					key2.getKeyInfo().itemKeyName(0).getKeyName()};
 			Arrays.sort(meta);
 			Arrays.sort(control);
 			assertTrue("Encountered unexpected key names", Arrays.equals(control, meta));
@@ -107,42 +113,86 @@ public class MetadataTests extends TestCase {
 		}
 	}
 
-    public void testBasicSAMLXML() {
+	public void testBasicSAMLXML() {
 
-        try {
-            Metadata metadata = new XMLMetadata(new File("src/conf/IQ-sites.xml").toURL().toString());
+		try {
+			Metadata metadata = new XMLMetadata(new File("src/conf/IQ-sites.xml").toURL().toString());
 
-            EntityDescriptor entity = metadata.lookup("urn:mace:inqueue:example.edu");
-            
-            assertNotNull("Unable to find test provider", entity);
-            assertEquals("Descriptor group is wrong.", entity.getEntitiesDescriptor().getName(),"urn:mace:inqueue");
-            
-            IDPSSODescriptor idp = entity.getIDPSSODescriptor(edu.internet2.middleware.shibboleth.common.XML.SHIB_NS);
-            AttributeAuthorityDescriptor aa = entity.getAttributeAuthorityDescriptor(XML.SAML11_PROTOCOL_ENUM);
-            SPSSODescriptor sp = entity.getSPSSODescriptor(XML.SAML11_PROTOCOL_ENUM);
-            
-            assertNotNull("Missing IdP provider role.", idp);
-            assertNotNull("Missing AA provider role.", aa);
-            assertNotNull("Missing SP provider role.", sp);
+			EntityDescriptor entity = metadata.lookup("urn:mace:inqueue:example.edu");
 
-            assertEquals("Incorrect assertion consumer service location.",
-                    ((Endpoint)sp.getAssertionConsumerServiceManager().getEndpoints().next()).getLocation(),
-                    "https://wayf.internet2.edu/Shibboleth.shire"
-                    );
+			assertNotNull("Unable to find test provider", entity);
+			assertEquals("Descriptor group is wrong.", entity.getEntitiesDescriptor().getName(), "urn:mace:inqueue");
 
-            Iterator keys = sp.getKeyDescriptors();
-            KeyDescriptor key = (KeyDescriptor)keys.next();
-            assertNotNull("Incorrect attribute requester key parsing.", key);
+			IDPSSODescriptor idp = entity.getIDPSSODescriptor(edu.internet2.middleware.shibboleth.common.XML.SHIB_NS);
+			AttributeAuthorityDescriptor aa = entity.getAttributeAuthorityDescriptor(XML.SAML11_PROTOCOL_ENUM);
+			SPSSODescriptor sp = entity.getSPSSODescriptor(XML.SAML11_PROTOCOL_ENUM);
 
-            String[] control = new String[]{"wayf.internet2.edu"};
-            String[] meta = new String[] {
-                    key.getKeyInfo().itemKeyName(0).getKeyName()
-                    };
-            Arrays.sort(meta);
-            Arrays.sort(control);
-            assertTrue("Encountered unexpected key names", Arrays.equals(control, meta));
-        } catch (Exception e) {
-            fail("Failed to correctly load metadata: " + e);
-        }
-    }
+			assertNotNull("Missing IdP provider role.", idp);
+			assertNotNull("Missing AA provider role.", aa);
+			assertNotNull("Missing SP provider role.", sp);
+
+			assertEquals("Incorrect assertion consumer service location.", ((Endpoint) sp
+					.getAssertionConsumerServiceManager().getEndpoints().next()).getLocation(),
+					"https://wayf.internet2.edu/Shibboleth.shire");
+
+			Iterator keys = sp.getKeyDescriptors();
+			KeyDescriptor key = (KeyDescriptor) keys.next();
+			assertNotNull("Incorrect attribute requester key parsing.", key);
+
+			String[] control = new String[]{"wayf.internet2.edu"};
+			String[] meta = new String[]{key.getKeyInfo().itemKeyName(0).getKeyName()};
+			Arrays.sort(meta);
+			Arrays.sort(control);
+			assertTrue("Encountered unexpected key names", Arrays.equals(control, meta));
+		} catch (Exception e) {
+			fail("Failed to correctly load metadata: " + e);
+		}
+	}
+
+	public void testInlineSAMLXML() {
+
+		try {
+
+			DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+			docFactory.setNamespaceAware(true);
+			Document placeHolder = docFactory.newDocumentBuilder().newDocument();
+
+			Element providerNode = placeHolder.createElementNS(IdPConfig.configNameSpace, "MetadataProvider");
+
+			Document xmlConfig = parser.parse(new InputSource(new FileInputStream("src/conf/IQ-sites.xml")));
+			Node metadataNode = placeHolder.importNode(xmlConfig.getDocumentElement(), true);
+			providerNode.appendChild(metadataNode);
+
+			Metadata metadata = new XMLMetadata(providerNode);
+
+			EntityDescriptor entity = metadata.lookup("urn:mace:inqueue:example.edu");
+
+			assertNotNull("Unable to find test provider", entity);
+			assertEquals("Descriptor group is wrong.", entity.getEntitiesDescriptor().getName(), "urn:mace:inqueue");
+
+			IDPSSODescriptor idp = entity.getIDPSSODescriptor(edu.internet2.middleware.shibboleth.common.XML.SHIB_NS);
+			AttributeAuthorityDescriptor aa = entity.getAttributeAuthorityDescriptor(XML.SAML11_PROTOCOL_ENUM);
+			SPSSODescriptor sp = entity.getSPSSODescriptor(XML.SAML11_PROTOCOL_ENUM);
+
+			assertNotNull("Missing IdP provider role.", idp);
+			assertNotNull("Missing AA provider role.", aa);
+			assertNotNull("Missing SP provider role.", sp);
+
+			assertEquals("Incorrect assertion consumer service location.", ((Endpoint) sp
+					.getAssertionConsumerServiceManager().getEndpoints().next()).getLocation(),
+					"https://wayf.internet2.edu/Shibboleth.shire");
+
+			Iterator keys = sp.getKeyDescriptors();
+			KeyDescriptor key = (KeyDescriptor) keys.next();
+			assertNotNull("Incorrect attribute requester key parsing.", key);
+
+			String[] control = new String[]{"wayf.internet2.edu"};
+			String[] meta = new String[]{key.getKeyInfo().itemKeyName(0).getKeyName()};
+			Arrays.sort(meta);
+			Arrays.sort(control);
+			assertTrue("Encountered unexpected key names", Arrays.equals(control, meta));
+		} catch (Exception e) {
+			fail("Failed to correctly load metadata: " + e);
+		}
+	}
 }
