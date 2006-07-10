da8fcd3058b9c0549671cbc82e30758d0d98ed61
wassa <wassa@ab3bd59b-922f-494d-bb5f-6f0a3c29deca>
2006-07-10 20:18:47 +0000

    Make OpenSAML MetadataProvider implementations loadable by the IdP configuration mechanisms.
    git-svn-id: https://subversion.switch.ch/svn/shibboleth/java-idp/trunk@1964 ab3bd59b-922f-494d-bb5f-6f0a3c29deca

---
+++ src/edu/internet2/middleware/shibboleth/metadata/ShibbolethConfigurableMetadataProvider.java
@@ -0,0 +1,80 @@
+
+package edu.internet2.middleware.shibboleth.metadata;
+
+import java.util.List;
+
+import javax.xml.namespace.QName;
+
+import org.opensaml.saml2.metadata.EntitiesDescriptor;
+import org.opensaml.saml2.metadata.EntityDescriptor;
+import org.opensaml.saml2.metadata.RoleDescriptor;
+import org.opensaml.saml2.metadata.provider.MetadataFilter;
+import org.opensaml.saml2.metadata.provider.MetadataProvider;
+import org.opensaml.saml2.metadata.provider.MetadataProviderException;
+import org.opensaml.xml.XMLObject;
+import org.w3c.dom.Element;
+
+/**
+ * Base class for <code>MetadataProvider</code> implementations that can be loaded based on the IdP runtime
+ * configuration. Implementors should create a constructor that accepts a configuration <code>Element</code> and sets
+ * the provider field.
+ * 
+ * @author Walter Hoehn
+ */
+public abstract class ShibbolethConfigurableMetadataProvider implements MetadataProvider {
+
+	protected org.opensaml.saml2.metadata.provider.MetadataProvider provider;
+
+	public ShibbolethConfigurableMetadataProvider(Element configuration) {
+
+	}
+
+	public boolean requireValidMetadata() {
+
+		return provider.requireValidMetadata();
+	}
+
+	public void setRequireValidMetadata(boolean requireValidMetadata) {
+
+		provider.setRequireValidMetadata(requireValidMetadata);
+
+	}
+
+	public MetadataFilter getMetadataFilter() {
+
+		return provider.getMetadataFilter();
+	}
+
+	public void setMetadataFilter(MetadataFilter newFilter) throws MetadataProviderException {
+
+		provider.setMetadataFilter(newFilter);
+
+	}
+
+	public EntityDescriptor getEntityDescriptor(String entityID) throws MetadataProviderException {
+
+		return provider.getEntityDescriptor(entityID);
+	}
+
+	public List<RoleDescriptor> getRole(String entityID, QName roleName) throws MetadataProviderException {
+
+		return provider.getRole(entityID, roleName);
+	}
+
+	public List<RoleDescriptor> getRole(String entityID, QName roleName, String supportedProtocol)
+			throws MetadataProviderException {
+
+		return provider.getRole(entityID, roleName, supportedProtocol);
+	}
+
+	public XMLObject getMetadata() throws MetadataProviderException {
+
+		return provider.getMetadata();
+	}
+
+	public EntitiesDescriptor getEntitiesDescriptor(String name) throws MetadataProviderException {
+
+		return provider.getEntitiesDescriptor(name);
+	}
+
+}
