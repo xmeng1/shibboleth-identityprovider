4e3852d115a8daf00bfbd7730818ab3e9b3076f5
lajoie <lajoie@ab3bd59b-922f-494d-bb5f-6f0a3c29deca>
2006-11-09 18:51:36 +0000

    AuthenticationManager class holder
    git-svn-id: https://subversion.switch.ch/svn/shibboleth/java-idp/trunk@2065 ab3bd59b-922f-494d-bb5f-6f0a3c29deca

---
+++ src/edu/internet2/middleware/shibboleth/idp/authn/AuthenticationManager.java
@@ -0,0 +1,10 @@
+package edu.internet2.middleware.shibboleth.idp.authn;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * Manager responsible for handling an authentication requests.
+ */
+public class AuthenticationManager extends HttpServlet {
+
+}
