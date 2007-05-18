3f4c519097fb9e406f23e3be5f8f3e7d1f930275
dmorr <dmorr@ab3bd59b-922f-494d-bb5f-6f0a3c29deca>
2007-05-18 19:28:15 +0000

    re-eclipsify the formatting
    git-svn-id: https://subversion.switch.ch/svn/shibboleth/java-idp/trunk@2198 ab3bd59b-922f-494d-bb5f-6f0a3c29deca

--- src/edu/internet2/middleware/shibboleth/idp/authn/impl/RemoteUserAuthServlet.java
+++ src/edu/internet2/middleware/shibboleth/idp/authn/impl/RemoteUserAuthServlet.java
@@ -31,47 +31,53 @@ import org.apache.log4j.Logger;
 import org.joda.time.DateTime;
 
 /**
- * This servlet should be protected by a filter which populates REMOTE_USER.
- * The serlvet will then set the remote user field in a LoginContext.
+ * This servlet should be protected by a filter which populates REMOTE_USER. The
+ * serlvet will then set the remote user field in a LoginContext.
  */
 public class RemoteUserAuthServlet extends HttpServlet {
 
-    private static final Logger log = Logger.getLogger(RemoteUserAuthServlet.class);
-    
-    public RemoteUserAuthServlet() {
-    }
+	private static final Logger log = Logger
+			.getLogger(RemoteUserAuthServlet.class);
 
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        
-        HttpSession httpSession = request.getSession();
-        
-        Object o = httpSession.getAttribute(LoginContext.LOGIN_CONTEXT_KEY);
-        if (!(o instanceof LoginContext)) {
-            log.error("RemoteUSerAuthServlet - Invalid login context object -- object is not an instance of LoginContext");
-            return; // where this will return to, I don't know.
-        }
-        
-        LoginContext loginContext = (LoginContext)o;
+	public RemoteUserAuthServlet() {
+	}
 
-        loginContext.setAuthenticationInstant(new DateTime());
-        
-        String remoteUser = request.getRemoteUser();
-        if (remoteUser == null || remoteUser.length() == 0) {
-            loginContext.setAuthenticationOK(false);;
-        } else {
-            loginContext.setAuthenticationOK(true);
-            loginContext.setUserID(remoteUser);
-        }
-        
-        // redirect the user back to the AuthenticationManager
-        try {
-            RequestDispatcher dispatcher =
-                    request.getRequestDispatcher(loginContext.getAuthenticationManagerURL());
-            dispatcher.forward(request, response);
-        } catch (ServletException ex) {
-            log.error("RemoteUserAuthServlet: Error redirecting back to AuthenticationManager", ex);
-        }
-    }
-        
-    
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws IOException {
+
+		HttpSession httpSession = request.getSession();
+
+		Object o = httpSession.getAttribute(LoginContext.LOGIN_CONTEXT_KEY);
+		if (!(o instanceof LoginContext)) {
+			log
+					.error("RemoteUSerAuthServlet - Invalid login context object -- object is not an instance of LoginContext");
+			return; // where this will return to, I don't know.
+		}
+
+		LoginContext loginContext = (LoginContext) o;
+
+		loginContext.setAuthenticationInstant(new DateTime());
+
+		String remoteUser = request.getRemoteUser();
+		if (remoteUser == null || remoteUser.length() == 0) {
+			loginContext.setAuthenticationOK(false);
+			;
+		} else {
+			loginContext.setAuthenticationOK(true);
+			loginContext.setUserID(remoteUser);
+		}
+
+		// redirect the user back to the AuthenticationManager
+		try {
+			RequestDispatcher dispatcher = request
+					.getRequestDispatcher(loginContext
+							.getAuthenticationManagerURL());
+			dispatcher.forward(request, response);
+		} catch (ServletException ex) {
+			log
+					.error(
+							"RemoteUserAuthServlet: Error redirecting back to AuthenticationManager",
+							ex);
+		}
+	}
 }
