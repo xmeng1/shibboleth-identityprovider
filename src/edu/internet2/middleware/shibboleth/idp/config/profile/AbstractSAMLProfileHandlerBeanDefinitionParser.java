f72d9fe51461d32298631bf9ff8c7ec6909ed365
lajoie <lajoie@ab3bd59b-922f-494d-bb5f-6f0a3c29deca>
2007-05-20 15:23:39 +0000

    Initial configuration code for profile handlers
    git-svn-id: https://subversion.switch.ch/svn/shibboleth/java-idp/trunk@2202 ab3bd59b-922f-494d-bb5f-6f0a3c29deca

---
+++ src/edu/internet2/middleware/shibboleth/idp/config/profile/AbstractSAMLProfileHandlerBeanDefinitionParser.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright [2007] [University Corporation for Advanced Internet Development, Inc.]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package edu.internet2.middleware.shibboleth.idp.config.profile;
+
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.w3c.dom.Element;
+
+import edu.internet2.middleware.shibboleth.common.config.profile.AbstractProfileHandlerBeanDefinitionParser;
+
+/**
+ * Base class for SAML profile handler configuration parsers.
+ */
+public abstract class AbstractSAMLProfileHandlerBeanDefinitionParser extends
+        AbstractProfileHandlerBeanDefinitionParser {
+
+    /** {@inheritDoc} */
+    protected void doParse(Element config, BeanDefinitionBuilder builder) {
+        super.doParse(config, builder);
+
+        builder.addPropertyReference("messageDecoderFactory", config.getAttributeNS(null, "messageDecoderFactoryId"));
+
+        builder.addPropertyReference("messageEncoderFactory", config.getAttributeNS(null, "messageEncoderFactoryId"));
+    }
+}
