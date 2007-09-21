f0f78cdf68c7543816edb77376f55078edf11f7e
rdw <rdw@ab3bd59b-922f-494d-bb5f-6f0a3c29deca>
2007-09-21 09:02:15 +0000

    Missing log statment
    git-svn-id: https://subversion.switch.ch/svn/shibboleth/java-idp/trunk@2404 ab3bd59b-922f-494d-bb5f-6f0a3c29deca

--- src/edu/internet2/middleware/shibboleth/idp/profile/saml1/AbstractSAML1ProfileHandler.java
+++ src/edu/internet2/middleware/shibboleth/idp/profile/saml1/AbstractSAML1ProfileHandler.java
@@ -341,6 +341,7 @@ public abstract class AbstractSAML1ProfileHandler extends AbstractSAMLProfileHan
             }
             requestContext.setFailureStatus(buildStatus(StatusCode.RESPONDER, null,
                     "Unable to construct NameIdentifier"));
+            log.error("No principal attribute supported encoding into the a supported name ID format.");
             throw new ProfileException("No principal attribute supported encoding into the a supported name ID format.");
         } catch (AttributeEncodingException e) {
             log.error("Unable to construct NameIdentifier", e);
